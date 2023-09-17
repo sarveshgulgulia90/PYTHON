5e7f68b5c2b0ca40405dec4209093bb8887da04a
SARVESH GULGULIA <141634846+sarveshgulgulia90@users.noreply.github.com>
2023-09-17 15:48:52 +0530

    Update lab5Q6.py

--- lab5Q6.py
+++ lab5Q6.py
@@ -1,6 +1,13 @@
 #taking inputs fromm user
 a = int(input(" Please Enter the First Value : "))
 b = int(input(" Please Enter the Second Value : "))
+#if nos are less than zero
+if a<0 or b<0:
+    print("Invalid")
+#if they are zero
+if a==0 or b==0:
+    print("please enter num greater than zero")
+    
 #initialising
 i = 1
 #looping till the hcf/gcd is found
